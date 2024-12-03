f32a324c88c19106912fe7007be7caf7c5d7b3e1
dbtmdgml98 <dbtmdgml98@naver.com>
2024-12-03 18:53:17 +0900

    feat: Store 생성 기능 구현

--- src/main/java/com/example/delivery_project/store/repository/StoreRepository.java
+++ src/main/java/com/example/delivery_project/store/repository/StoreRepository.java
@@ -1,5 +1,11 @@
 package com.example.delivery_project.store.repository;
 
-public interface StoreRepository {
+import com.example.delivery_project.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    Store findByName(String name);
 }
