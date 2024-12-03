a64da55cb0dbeeaf44d4b2d547dead48b0a47c89
Weseunghyun <bible091192@gmail.com>
2024-12-03 16:58:48 +0900

    refactor: TimeBaseEntity 수정

--- src/main/java/com/example/delivery_project/common/entity/TimeBaseEntity.java
+++ src/main/java/com/example/delivery_project/common/entity/TimeBaseEntity.java
@@ -1,17 +1,19 @@
 package com.example.delivery_project.common.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Getter
 @MappedSuperclass
-@EnableJpaAuditing
-public class TimeBaseEntity {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeBaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
