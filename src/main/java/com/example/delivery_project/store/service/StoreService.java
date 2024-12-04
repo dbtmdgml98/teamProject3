3c429333675e7b1be993c79c47b281bd77c0f4d6
dbtmdgml98 <dbtmdgml98@naver.com>
2024-12-04 17:18:43 +0900

    feat: 가게 생성시 예외 추가2
    사장님은 폐업 상태가 아닌 가게를 최대 3개까지만 운영 가능

--- src/main/java/com/example/delivery_project/store/service/StoreService.java
+++ src/main/java/com/example/delivery_project/store/service/StoreService.java
@@ -6,6 +6,7 @@ import com.example.delivery_project.store.dto.StoreRequestDto;
 import com.example.delivery_project.store.dto.StoreResponseDto;
 import com.example.delivery_project.store.dto.UpdateStoreStatusResponseDto;
 import com.example.delivery_project.store.entity.Store;
+import com.example.delivery_project.store.entity.StoreStatus;
 import com.example.delivery_project.store.repository.StoreRepository;
 import com.example.delivery_project.user.entity.Authority;
 import com.example.delivery_project.user.entity.User;
@@ -27,22 +28,23 @@ public class StoreService {
 
     public StoreResponseDto createStore(StoreRequestDto storeRequestDto, Authority authority, Long userId) {
 
-        if (userId == null) {
-            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "해당 유저가 존재하지 않습니다.");
-        }
-
         // 사징님이 아닌 경우
         if (!authority.equals(Authority.OWNER)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "사장님 권한을 가진 유저만 가게를 만들 수 있습니다.");
         }
 
         // 가게 이름이 이미 존재하는 경우
-        Store findName = storeRepository.findByName(storeRequestDto.getName());
-        if (findName != null) {
+        Store findStoreByName = storeRepository.findByName(storeRequestDto.getName());
+        if (findStoreByName != null) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "가게 이름이 이미 존재합니다.");
         }
 
+        // 폐업 상태가 아닌 가게가 3개 초과일 경우
         User findUser = userRepository.findByIdOrElseThrow(userId);
+        Long countOpenStore = storeRepository.countByUserAndStoreStatus(findUser, StoreStatus.OPEN);
+        if (countOpenStore > 2) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "사장님은 폐업 상태가 아닌 가게를 최대 3개까지만 운영할 수 있습니다.");
+        }
 
         Store store = new Store(storeRequestDto.getName(), storeRequestDto.getOpenTime(), storeRequestDto.getCloseTime(), storeRequestDto.getMinimumOrderPrice(), findUser);
         Store savedStore = storeRepository.save(store);
