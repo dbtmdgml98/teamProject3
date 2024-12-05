e13af5c42bb1b66dc5cd28d1b8b30afa32a7c506
dbtmdgml98 <dbtmdgml98@naver.com>
2024-12-05 20:30:07 +0900

    feat: Review 생성시 예외 추가
    배달 완료 되지 않은 주문을 리뷰하려는 경우 예외 추가

--- src/main/java/com/example/delivery_project/review/service/ReviewService.java
+++ src/main/java/com/example/delivery_project/review/service/ReviewService.java
@@ -1,13 +1,16 @@
 package com.example.delivery_project.review.service;
 
 import com.example.delivery_project.order.entity.Order;
+import com.example.delivery_project.order.entity.OrderStatus;
 import com.example.delivery_project.order.repository.OrderRepository;
 import com.example.delivery_project.review.dto.ReviewRequestDto;
 import com.example.delivery_project.review.dto.ReviewResponseDto;
 import com.example.delivery_project.review.entity.Review;
 import com.example.delivery_project.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +22,11 @@ public class ReviewService {
     public ReviewResponseDto createReview(ReviewRequestDto reviewRequestDto) {
 
         Order foundOrder = orderRepository.findByIdOrElseThrow(reviewRequestDto.getOrderId());
+
+        // 배달 완료 되지 않은 주문일 경우
+        if (!foundOrder.getOrderStatus().equals(OrderStatus.DELIVERY_FINISHED)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "배달 완료 되지 않은 주문은 리뷰를 작성할 수 없습니다.");
+        }
 
         Review review = new Review(foundOrder, reviewRequestDto.getStarPoint(), reviewRequestDto.getContent());
 
