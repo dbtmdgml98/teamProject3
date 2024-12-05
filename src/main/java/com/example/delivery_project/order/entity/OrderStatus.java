02d089e190cc719c67ac2af9afb62c7db7b2028e
kjs0406 <rnjswlstjr19089@gmail.com>
2024-12-05 11:02:37 +0900

    enum 생성

---
+++ src/main/java/com/example/delivery_project/order/entity/OrderStatus.java
@@ -0,0 +1,20 @@
+package com.example.delivery_project.order.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderStatus {
+    ORDER_FINISHED("주문 완료"),
+    ACCEPT_ORDER("주문 수락"),
+    NOW_COOKING("조리 중"),
+    COOKING_FINISHED("조리 완료"),
+    NOW_DELIVERY("배달 중"),
+    DELIVERY_FINISHED("배달 완료");
+
+    OrderStatus(String description) {
+        this.description = description;
+    }
+
+    private final String description;
+
+}
